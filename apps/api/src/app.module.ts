896c7f8c43c5cc44d8be43221407cb87d6eb9572
soylltari <mmmsribranets@gmail.com>
2025-11-16 16:30:44 +0200

    feat: implement Gmail integration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '@common/prisma';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, MailModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
